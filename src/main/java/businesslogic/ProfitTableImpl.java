e2de4af6a6163bdd026462656dd07c4aa61e648b
151250055 <151250055@smail.nju.edu.cn>
2017-08-07 15:42:15 +0800

    利润表接口以及VO

---
+++ src/main/java/businesslogic/ProfitTableImpl.java
@@ -0,0 +1,29 @@
+package businesslogic;
+
+import businesslogicservice.ProfitTableService;
+import vopo.BusinessIncomeVo;
+import vopo.BusinessProfit_And_OtherVo;
+
+public class ProfitTableImpl implements ProfitTableService{
+
+	public BusinessIncomeVo BusinessIncome_period(String time) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public BusinessIncomeVo BusinessIncome_year(String time) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public BusinessProfit_And_OtherVo BusinessProfit_And_OtherVo_period(String time) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public BusinessProfit_And_OtherVo BusinessProfit_And_OtherVo_year(String time) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
---
+++ src/main/java/businesslogicservice/ProfitTableService.java
@@ -0,0 +1,39 @@
+package businesslogicservice;
+
+import vopo.BusinessIncomeVo;
+import vopo.BusinessProfit_And_OtherVo;
+
+/**
+ * 
+ * @author hyf
+ *
+ */
+public interface ProfitTableService {
+	/**
+	 * 
+	 * @param time 某年某期 yyyy-xx
+	 * @return 返回该期的营业收入相关数据
+	 */
+	public BusinessIncomeVo BusinessIncome_period(String time);
+	
+	/**
+	 * 
+	 * @param time 年份 yyyy
+	 * @return 返回该年的营业收入相关数据
+	 */
+	public BusinessIncomeVo BusinessIncome_year(String time);
+	
+	/**
+	 * @param time 某年某期 yyyy-xx
+	 * @return 返回该期包括营业利润、利润总额、净利润的信息
+	 */
+	public BusinessProfit_And_OtherVo BusinessProfit_And_OtherVo_period(String time);
+	
+	/**
+	 * @param time 年份 yyyy
+	 * @return 返回该年包括营业利润、利润总额、净利润的信息
+	 */
+	public BusinessProfit_And_OtherVo BusinessProfit_And_OtherVo_year(String time);
+	
+	
+}
