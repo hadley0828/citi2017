86f27b68d2832cdae61d2b35cbe02e49e988c7c7
hadley Zhang <378798874@qq.com>
2017-08-13 13:40:57 +0800

    完成voucherDataService接口中的全部方法

--- src/main/java/businesslogicservice/VoucherBlService.java
+++ src/main/java/businesslogicservice/VoucherBlService.java
@@ -1,10 +1,7 @@
 package businesslogicservice;
 
 import po.VoucherPO;
-import vo.voucher.AmountTotalVo;
-import vo.voucher.VoucherSearchVo;
-import vo.voucher.VoucherTemplateVo;
-import vo.voucher.VoucherVo;
+import vo.voucher.*;
 
 import java.util.ArrayList;
 
@@ -30,17 +27,17 @@ public interface VoucherBlService {
 
     /**
      * 添加一个新的凭证 该凭证的id不能与数据库中的重复 若重复则返回false
-     * @param voucherPO
+     * @param voucherVo
      * @return
      */
-    public boolean saveOneVoucher(VoucherPO voucherPO);
+    public boolean saveOneVoucher(VoucherVo voucherVo);
 
     /**
      * 获得当前凭证的合计一行的数据
-     * @param voucherPO
+     * @param amountVoArrayList
      * @return
      */
-    public AmountTotalVo getVoucherTotal(VoucherPO voucherPO);
+    public AmountTotalVo getVoucherTotal(ArrayList<VoucherAmountVo> amountVoArrayList);
 
     /**
      * 获得当前会计科目的余额
@@ -56,6 +53,14 @@ public interface VoucherBlService {
      * @return
      */
     public double getNewSubjectBalance(double beforeNumber,double changeNumber);
+
+    /**
+     * 改变会计科目的余额 在保存凭证的时候需要调用
+     * @param subjectId
+     * @param newNumber
+     * @return
+     */
+    public boolean changeSubjectBalance(String subjectId,double newNumber);
 
     /**
      * 根据模板编号获得相对应的凭证模板
@@ -105,7 +110,42 @@ public interface VoucherBlService {
      */
     public ArrayList<VoucherVo> importFromExcel(String filePath);
 
+    /**
+     * 修改一条凭证信息 需要判断凭证编号是否发生改变 改变的话需要判断是否在数据库中已经存在该编号
+     * @param voucherId 修改之前的凭证编号
+     * @param voucherVo 修改之后的凭证信息
+     * @return
+     */
+    public boolean amendOneVoucher(String voucherId,VoucherVo voucherVo);
 
-    //TODO 打印凭证并且对打印进行设置    对一条凭证进行处理 修改!!!  删除  复制  插入    可以新增一条会计科目
+    /**
+     * 删除指定的凭证信息
+     * @param voucherId 需要删除的凭证编号
+     * @return
+     */
+    public boolean deleteOneVoucher(String voucherId);
+
+    /**
+     * 复制选定的凭证信息
+     * @param voucherId
+     * @return
+     */
+    public VoucherVo copyOneVoucher(String voucherId);
+
+    /**
+     * 添加一个新的会计科目
+     * @return
+     */
+    public boolean addOneSubject();
+
+    /**
+     * 根据输入的凭证字获得当前的凭证号 按照数字的顺序
+     * @param voucherCharacter
+     * @return
+     */
+    public int getCurrentNumber(String voucherCharacter);
+
+
+    //TODO 打印凭证并且对打印进行设置    对一条凭证进行处理 修改!!!  删除  复制  插入(未实现)    可以新增一条会计科目
 
 }
