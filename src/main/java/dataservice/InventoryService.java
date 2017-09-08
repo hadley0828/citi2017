0151f1e2f1fa6294608165fe98c5e2a688c792a6
FeiHuitong <bastian210@163.com>
2017-09-08 08:33:40 +0800

    更新库存管理数据层接口

--- src/main/java/dataservice/InventoryService.java
+++ src/main/java/dataservice/InventoryService.java
@@ -10,62 +10,70 @@ import java.util.ArrayList;
 public interface InventoryService {
     /**
      * 获取当前时间某供应商的原材料的库存
-     * @param name 供应商名称
+     * @param voucher_id 凭证编号
+     * @param raw_material_name 原材料种类
      * @return
      */
-    public int getRawInventoryBySupplier(String name);
+    public int getRawInventoryBySupplier(String voucher_id,String raw_material_name);
 
     /**
      * 更新供应商的原材料库存
-     * @param name 供应商名称
+     * @param voucher_id 凭证编号
+     * @param raw_material_name 原材料种类
      * @param inventory 库存量
      * @return
      */
-    public boolean setSupplierRawInventory(String name, int inventory);
+    public boolean setSupplierRawInventory(String voucher_id,String raw_material_name, int inventory);
 
     /**
      * 获取当前时间某生产商的原材料库存
-     * @param name 生产商名称
+     * @param voucher_id 凭证编号
+     * @param raw_material_name 原材料种类
      * @return
      */
-    public int getRawInventoryByProducer(String name);
+    public int getRawInventoryByProducer(String voucher_id,String raw_material_name);
 
     /**
      * 更新生产商的原材料库存
-     * @param name 生产商名称
+     * @param voucher_id 凭证编号
+     * @param raw_material_name 原材料种类
      * @param inventory 库存量
      * @return
      */
-    public boolean setProducerRawInventory(String name, int inventory);
+    public boolean setProducerRawInventory(String voucher_id,String raw_material_name, int inventory);
 
     /**
      * 获取当前时间某生产商的产品库存
-     * @param name 生产商名称
+     * @param voucher_id 凭证编号
+     * @param product_name 原材料种类
      * @return
      */
-    public int getProductInventoryByProducer(String name);
+    public int getProductInventoryByProducer(String voucher_id,String product_name);
 
     /**
      * 更新生产商的产品库存
-     * @param name 生产商名称
+     * @param voucher_id 凭证编号
+     * @param product_name 原材料种类
      * @param inventory 库存量
      * @return
      */
-    public boolean setProducterProductInventory(String name, int inventory);
+    public boolean setProducterProductInventory(String voucher_id,String product_name, int inventory);
 
     /**
      * 获取当前时间某分销商的产品库存
-     * @param name 分销商名称
+     * @param voucher_id 凭证编号
+     * @param product_name 原材料种类
      * @return
      */
-    public int getProductInventoryByDistributor(String name);
+    public int getProductInventoryByDistributor(String voucher_id,String product_name);
 
     /**
      * 更新分销商的产品库存
-     * @param name 分销商名称
+     * @param voucher_id 凭证编号
+     * @param product_name 原材料种类
      * @param inventory 库存量
      * @return
      */
-    public boolean setDistributorProductInventory(String name, int inventory);
+    public boolean setDistributorProductInventory(String voucher_id,String product_name, int inventory);
 
 }
