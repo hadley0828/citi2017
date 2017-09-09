62a30572a4c97c2cb49faf8803e7584d107f1a3e
hadley Zhang <378798874@qq.com>
2017-09-09 15:46:43 +0800

    完成账簿功能的接口

--- src/main/java/businesslogic/AccountBooksBlImpl.java
+++ src/main/java/businesslogic/AccountBooksBlImpl.java
@@ -1,10 +1,42 @@
 package businesslogic;
 
 import businesslogicservice.AccountBooksBlService;
+import vo.accountBook.*;
+
+import java.util.ArrayList;
 
 /**
  * Created by zhangzy on 2017/8/7 下午10:45
  */
 public class AccountBooksBlImpl implements AccountBooksBlService {
 
+    @Override
+    public ArrayList<String> getAllExistedSubjectId(String factoryId) {
+        return null;
+    }
+
+    @Override
+    public DetailAccountVo getOneSubjectDetail(String subjectId, BookSearchVo searchVo, String factoryId) {
+        return null;
+    }
+
+    @Override
+    public ArrayList<TotalAccountVo> getAllSubjectTotal(BookSearchVo searchVo, String factoryId) {
+        return null;
+    }
+
+    @Override
+    public TotalAccountVo getOneSubjectTotal(String subjectId, BookSearchVo searchVo, String factoryId) {
+        return null;
+    }
+
+    @Override
+    public ArrayList<BalanceTableOneClause> getBalanceTableAllClauses(BookSearchVo searchVo, String factoryId) {
+        return null;
+    }
+
+    @Override
+    public ArrayList<GatherTableOneClause> getGatherTableAllClauses(BookSearchVo searchVo, String factoryId) {
+        return null;
+    }
 }
--- src/main/java/businesslogicservice/AccountBooksBlService.java
+++ src/main/java/businesslogicservice/AccountBooksBlService.java
@@ -1,7 +1,6 @@
 package businesslogicservice;
 
-import vo.accountBook.BookSearchVo;
-import vo.accountBook.DetailAccountVo;
+import vo.accountBook.*;
 
 import java.util.ArrayList;
 
@@ -11,21 +10,71 @@ import java.util.ArrayList;
  */
 public interface AccountBooksBlService {
 
-    //明细账
+    /**
+     * 明细账
+     */
+
+    /**
+     * 获得全部有数据记录的科目编号
+     * @param factoryId
+     * @return
+     */
     public ArrayList<String> getAllExistedSubjectId(String factoryId);
 
+    /**
+     * 获得一个科目的明细账
+     * 默认状态是会计期间是当前的期间 科目级别是1到1
+     * @param subjectId
+     * @param searchVo
+     * @param factoryId
+     * @return
+     */
     public DetailAccountVo getOneSubjectDetail(String subjectId, BookSearchVo searchVo,String factoryId);
 
+    /**
+     * 总账
+     */
+
+    /**
+     * 获得全部科目的总账
+     * @param searchVo
+     * @param factoryId
+     * @return
+     */
+    public ArrayList<TotalAccountVo> getAllSubjectTotal(BookSearchVo searchVo,String factoryId);
+
+    /**
+     * 获得单个科目的总账
+     * @param subjectId
+     * @param searchVo
+     * @param factoryId
+     * @return
+     */
+    public TotalAccountVo getOneSubjectTotal(String subjectId,BookSearchVo searchVo,String factoryId);
+
+    /**
+     * 科目余额表
+     */
+
+    /**
+     * 获取部分科目的科目余额表
+     * @param searchVo
+     * @param factoryId
+     * @return
+     */
+    public ArrayList<BalanceTableOneClause> getBalanceTableAllClauses(BookSearchVo searchVo,String factoryId);
 
 
+    /**
+     * 科目汇总表
+     */
 
-    //总账
-
-
-    //科目余额表
-
-
-    //科目汇总表
-
+    /**
+     * 获取部分科目的科目汇总表
+     * @param searchVo
+     * @param factoryId
+     * @return
+     */
+    public ArrayList<GatherTableOneClause> getGatherTableAllClauses(BookSearchVo searchVo,String factoryId);
 
 }
