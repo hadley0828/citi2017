bcfb46a8df04ce59343f6e1199e5fe832333eaeb
loohaze <loohaze529@gmail.com>
2017-09-11 02:50:54 +0800

    InventoryService 全部实现 已测试

--- src/main/java/data/InventoryServiceImpl.java
+++ src/main/java/data/InventoryServiceImpl.java
@@ -6,6 +6,7 @@ import po.Inventory.InventoryRawMaterialItemPO;
 import po.Inventory.ProductSafeInventoryPo;
 import po.Inventory.RawMaterialSafeInventoryPo;
 
+import java.sql.Timestamp;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
@@ -82,67 +83,181 @@ public class InventoryServiceImpl implements InventoryService{
 
         ArrayList<InventoryRawMaterialItemPO> list = new ArrayList<>();
 
-        String sql = "select * from inventory_material where datetime<=? and company_id =? group by material_variety order by datetime";
+        ArrayList<String> varietyList = getRawMaterialVarietyList(company_id);
+
+        for (String variety : varietyList){
+            String sql = "select * from inventory_material where datetime<=? and company_id =? and material_variety =? order by datetime";
+            ArrayList<Map<String,Object>> maps = sqlManager.queryMulti(sql,new Object[]{time,company_id,variety});
+
+            for (Map<String,Object> map : maps){
+                list.add(getInventoryRawMaterialItemPOByMap(map));
+            }
+        }
+
         sqlManager.releaseAll();
-        return null;
+        return list;
     }
 
     @Override
     public ArrayList<InventoryProductItemPO> getProductInventoryItem(String company_id, String time) {
-        return null;
+        sqlManager.getConnection();
+
+        ArrayList<InventoryProductItemPO> list = new ArrayList<>();
+
+        ArrayList<String> varietyList = getProductVarietyList(company_id);
+
+        for (String variety : varietyList){
+            String sql = "select * from inventory_product where datetime<=? and company_id =? and product_variety=? order by datetime";
+            ArrayList<Map<String,Object>> maps = sqlManager.queryMulti(sql,new Object[]{time,company_id,variety});
+
+            for (Map<String,Object> map : maps){
+                list.add(getInventoryProductItemPOByMap(map));
+            }
+        }
+
+        sqlManager.releaseAll();
+        return list;
     }
 
     @Override
     public ArrayList<InventoryRawMaterialItemPO> getRawMaterialInventoryItemByVariety(String company_id, String time, String variety) {
-        return null;
+        sqlManager.getConnection();
+
+        ArrayList<InventoryRawMaterialItemPO> list = new ArrayList<>();
+
+        String sql = "select * from inventory_material where datetime<=? and company_id=? and material_variety=? order by datetime";
+        ArrayList<Map<String,Object>> maps = sqlManager.queryMulti(sql,new Object[]{time,company_id,variety});
+
+        for (Map<String,Object> map : maps){
+            list.add(getInventoryRawMaterialItemPOByMap(map));
+        }
+
+        sqlManager.releaseAll();
+        return list;
     }
 
     @Override
     public ArrayList<InventoryProductItemPO> getProductInventoryItemByVariety(String company_id, String time, String variety) {
-        return null;
+        sqlManager.getConnection();
+
+        ArrayList<InventoryProductItemPO> list = new ArrayList<>();
+
+        String sql = "select * from inventory_product where datetime<=? and company_id=? and product_variety=? order by datetime";
+        ArrayList<Map<String,Object>> maps = sqlManager.queryMulti(sql,new Object[]{time,company_id,variety});
+
+        for (Map<String,Object> map : maps){
+            list.add(getInventoryProductItemPOByMap(map));
+        }
+
+        sqlManager.releaseAll();
+        return list;
     }
 
     @Override
     public void SaveRawMaterialSafeInventory(ArrayList<RawMaterialSafeInventoryPo> list) {
-
+        sqlManager.getConnection();
+        for (RawMaterialSafeInventoryPo po : list){
+            insertOneRawMaterialSafeInventoryPo(po);
+        }
+        sqlManager.releaseAll();
     }
 
     @Override
     public void SaveProductSafeInventory(ArrayList<ProductSafeInventoryPo> list) {
-
+        sqlManager.getConnection();
+        for (ProductSafeInventoryPo po : list){
+            insertOneProductSafeInventoryPo(po);
+        }
+        sqlManager.releaseAll();
     }
 
     @Override
     public ArrayList<String> getAllRawMaterialVariety(String company_id) {
-        return null;
+        sqlManager.getConnection();
+        ArrayList<String> varietyList = getRawMaterialVarietyList(company_id);
+        sqlManager.releaseAll();
+        return varietyList;
     }
 
     @Override
     public ArrayList<String> getAllProductVariety(String company_id) {
-        return null;
+        sqlManager.getConnection();
+        ArrayList<String> varietyList = getProductVarietyList(company_id);
+        sqlManager.releaseAll();
+        return varietyList;
     }
 
     @Override
     public ArrayList<RawMaterialSafeInventoryPo> getAllRawMaterialSafeInventory(String company_id) {
-        return null;
+        sqlManager.getConnection();
+
+        ArrayList<RawMaterialSafeInventoryPo> list = new ArrayList<>();
+
+        String sql = "select * from safe_inventory_material where company_id=?";
+        ArrayList<Map<String,Object>> maps = sqlManager.queryMulti(sql,new Object[]{company_id});
+
+        for (Map<String,Object> map : maps){
+            list.add(getRawMaterialSafeInventoryPoByMap(map));
+        }
+        sqlManager.releaseAll();
+        return list;
     }
 
     @Override
     public ArrayList<ProductSafeInventoryPo> getAllProductSafeInventory(String company_id) {
-        return null;
+        sqlManager.getConnection();
+
+        ArrayList<ProductSafeInventoryPo> list = new ArrayList<>();
+
+        String sql = "select * from safe_inventory_product where company_id=?";
+        ArrayList<Map<String,Object>> maps = sqlManager.queryMulti(sql,new Object[]{company_id});
+
+        for (Map<String,Object> map : maps){
+            list.add(getProductSafeInventoryPo(map));
+        }
+        sqlManager.releaseAll();
+        return list;
     }
 
     @Override
     public int getRawInventory(String company_id, String raw_material_variety) {
-        return 0;
+        sqlManager.getConnection();
+
+        String sql = "select safety_stock from safe_inventory_material where company_id=? and variety=?";
+        Map<String,Object> map = sqlManager.querySimple(sql,new Object[]{company_id,raw_material_variety});
+
+        try{
+            int inventory = Integer.parseInt(map.get("safety_stock").toString());
+            sqlManager.releaseAll();
+            return inventory;
+        }catch (Exception e){
+            sqlManager.releaseAll();
+            return 0;
+        }
     }
 
     @Override
     public int getProductInventory(String company_id, String product_variety) {
-        return 0;
+        sqlManager.getConnection();
+
+        String sql = "select safety_stock from safe_inventory_product where company_id=? and variety=?";
+        Map<String,Object> map = sqlManager.querySimple(sql,new Object[]{company_id,product_variety});
+
+        try {
+            int inventory = Integer.parseInt(map.get("safety_stock").toString());
+            sqlManager.releaseAll();
+            return inventory;
+        }catch (Exception e){
+            sqlManager.releaseAll();
+            return 0;
+        }
     }
 
 
+    /**
+     * 数据库单条InventoryRawMaterialItemPO插入
+     * @param po
+     */
     private void insertOneInventoryRawMaterialItemPO(InventoryRawMaterialItemPO po){
         List<Object> params = new ArrayList<>();
 
@@ -166,6 +281,10 @@ public class InventoryServiceImpl implements InventoryService{
         sqlManager.executeUpdateByList(sql,params);
     }
 
+    /**
+     * 数据库单条InventoryProductItemPO插入
+     * @param po
+     */
     private void insertOneInventoryProductItemPO(InventoryProductItemPO po){
         List<Object> params = new ArrayList<>();
 
@@ -186,5 +305,151 @@ public class InventoryServiceImpl implements InventoryService{
 
         String sql = sqlManager.appendSQL("insert into inventory_product values",params.size());
         sqlManager.executeUpdateByList(sql,params);
+    }
+
+    /**
+     * 数据库单条RawMaterialSafeInventoryPo插入
+     * @param po
+     */
+    private void insertOneRawMaterialSafeInventoryPo(RawMaterialSafeInventoryPo po){
+        List<Object> params = new ArrayList<>();
+
+        params.add(po.getCompany_id());
+        params.add(po.getRaw_material_variety());
+        params.add(po.getSafe_inventory());
+
+        String sql = sqlManager.appendSQL("insert into safe_inventory_material values",params.size());
+        sqlManager.executeUpdateByList(sql,params);
+    }
+
+    /**
+     * 数据库单条ProductSafeInventoryPo插入
+     * @param po
+     */
+    private void insertOneProductSafeInventoryPo(ProductSafeInventoryPo po){
+        List<Object> params = new ArrayList<>();
+
+        params.add(po.getCompany_id());
+        params.add(po.getProduct_variety());
+        params.add(po.getSafe_inventory());
+
+        String sql = sqlManager.appendSQL("insert into safe_inventory_product values",params.size());
+        sqlManager.executeUpdateByList(sql,params);
+    }
+
+    /**
+     * 通过数据库获得map转为InventoryRawMaterialItemPO
+     * @param map
+     * @return
+     */
+    private InventoryRawMaterialItemPO getInventoryRawMaterialItemPOByMap(Map<String,Object> map){
+        InventoryRawMaterialItemPO po = new InventoryRawMaterialItemPO();
+
+        po.setSheetID(Integer.parseInt(map.get("sheet_id").toString()));
+        po.setListID(Integer.parseInt(map.get("list_id").toString()));
+        po.setCompanyID(map.get("company_id").toString());
+        po.setVariety(map.get("material_variety").toString());
+        po.setDate(Timestamp.valueOf(map.get("datetime").toString()));
+        po.setDeliveryOntime(Boolean.valueOf(map.get("is_delivery_ontime").toString()));
+        po.setReturn(Boolean.valueOf(map.get("is_return").toString()));
+        po.setInputNum(Integer.parseInt(map.get("input_num").toString()));
+        po.setInputUnitPrice(Double.parseDouble(map.get("input_unit_price").toString()));
+        po.setInputTotalPrice(Double.parseDouble(map.get("input_account").toString()));
+        po.setOutputNum(Integer.parseInt(map.get("output_num").toString()));
+        po.setOutputUnitPrice(Double.parseDouble(map.get("output_unit_price").toString()));
+        po.setOutputTotalPrice(Double.parseDouble(map.get("output_account").toString()));
+        po.setRemainNum(Integer.parseInt(map.get("balance_num").toString()));
+
+        return po;
+    }
+
+    /**
+     * 通过数据库获得map转为InventoryProductItemPO
+     * @param map
+     * @return
+     */
+    private InventoryProductItemPO getInventoryProductItemPOByMap(Map<String,Object> map){
+        InventoryProductItemPO po = new InventoryProductItemPO();
+
+        po.setSheetID(Integer.parseInt(map.get("sheet_id").toString()));
+        po.setListID(Integer.parseInt(map.get("list_id").toString()));
+        po.setCompanyID(map.get("company_id").toString());
+        po.setVariety(map.get("product_variety").toString());
+        po.setDate(Timestamp.valueOf(map.get("datetime").toString()));
+        po.setDeliveryOntime(Boolean.valueOf(map.get("is_delivery_ontime").toString()));
+        po.setReturn(Boolean.valueOf(map.get("is_return").toString()));
+        po.setInputNum(Integer.parseInt(map.get("input_num").toString()));
+        po.setInputUnitPrice(Double.parseDouble(map.get("input_unit_price").toString()));
+        po.setInputTotalPrice(Double.parseDouble(map.get("input_account").toString()));
+        po.setOutputNum(Integer.parseInt(map.get("output_num").toString()));
+        po.setOutputUnitPrice(Double.parseDouble(map.get("output_unit_price").toString()));
+        po.setOutputTotalPrice(Double.parseDouble(map.get("output_account").toString()));
+        po.setRemainNum(Integer.parseInt(map.get("balance_num").toString()));
+
+        return po;
+    }
+
+    /**
+     * 通过数据库获得map转为RawMaterialSafeInventoryPo
+     * @param map
+     * @return
+     */
+    private RawMaterialSafeInventoryPo getRawMaterialSafeInventoryPoByMap(Map<String,Object> map){
+        RawMaterialSafeInventoryPo po = new RawMaterialSafeInventoryPo();
+
+        po.setCompany_id(map.get("company_id").toString());
+        po.setRaw_material_variety(map.get("variety").toString());
+        po.setSafe_inventory(Integer.parseInt(map.get("safety_stock").toString()));
+
+        return po;
+    }
+
+    /**
+     * 通过数据库获得map转为ProductSafeInventoryPo
+     * @return
+     */
+    private ProductSafeInventoryPo getProductSafeInventoryPo(Map<String,Object> map){
+        ProductSafeInventoryPo po = new ProductSafeInventoryPo();
+
+        po.setCompany_id(map.get("company_id").toString());
+        po.setProduct_variety(map.get("variety").toString());
+        po.setSafe_inventory(Integer.parseInt(map.get("safety_stock").toString()));
+
+        return po;
+    }
+    /**
+     * 获得公司所有原材料种类
+     * @param company_id
+     * @return
+     */
+    private ArrayList<String> getRawMaterialVarietyList(String company_id){
+        ArrayList<String> list = new ArrayList<>();
+
+        String sql = "select distinct material_variety from inventory_material where company_id=?";
+        ArrayList<Map<String,Object>> maps = sqlManager.queryMulti(sql,new Object[]{company_id});
+
+        for (Map<String,Object> map : maps){
+            list.add(map.get("material_variety").toString());
+        }
+
+        return list;
+    }
+
+    /**
+     * 获得公司所有产品种类
+     * @param company_id
+     * @return
+     */
+    private ArrayList<String> getProductVarietyList(String company_id){
+        ArrayList<String> list = new ArrayList<>();
+
+        String sql = "select distinct product_variety from inventory_product where company_id=?";
+        ArrayList<Map<String,Object>> maps = sqlManager.queryMulti(sql,new Object[]{company_id});
+
+        for (Map<String,Object> map : maps){
+            list.add(map.get("product_variety").toString());
+        }
+
+        return list;
     }
 }
--- src/main/test/dataTest/InventoryDataServiceImplTest.java
+++ src/main/test/dataTest/InventoryDataServiceImplTest.java
@@ -6,6 +6,8 @@ import org.junit.Before;
 import org.junit.Test;
 import po.Inventory.InventoryProductItemPO;
 import po.Inventory.InventoryRawMaterialItemPO;
+import po.Inventory.ProductSafeInventoryPo;
+import po.Inventory.RawMaterialSafeInventoryPo;
 
 import java.sql.Timestamp;
 import java.util.ArrayList;
@@ -56,5 +58,158 @@ public class InventoryDataServiceImplTest {
         list.add(po1);
 
         inventoryService.SaveProductInventoryItem(list);
+    }
+
+    @Test
+    public void testgetRawMaterialInventoryItem(){
+        ArrayList<InventoryRawMaterialItemPO> list = new ArrayList<>();
+        list = inventoryService.getRawMaterialInventoryItem("001","2017-12-12");
+
+        for (InventoryRawMaterialItemPO po : list){
+            print(po);
+        }
+    }
+
+    @Test
+    public void testgetProductInventoryItem(){
+        ArrayList<InventoryProductItemPO> list = new ArrayList<>();
+        list = inventoryService.getProductInventoryItem("001","2017-12-12");
+
+        for (InventoryProductItemPO po : list){
+            print(po);
+        }
+    }
+
+    @Test
+    public void testgetRawMaterialInventoryItemByVariety(){
+        ArrayList<InventoryRawMaterialItemPO> list = inventoryService.getRawMaterialInventoryItemByVariety("001","2017-12-12","wood");
+        for (InventoryRawMaterialItemPO po : list){
+            print(po);
+        }
+    }
+
+    @Test
+    public void testgetProductInventoryItemByVariety(){
+        ArrayList<InventoryProductItemPO> list = inventoryService.getProductInventoryItemByVariety("001","2017-12-12","pen");
+        for (InventoryProductItemPO po : list){
+            print(po);
+        }
+    }
+
+    @Test
+    public void testSaveRawMaterialSafeInventory(){
+        RawMaterialSafeInventoryPo po1 = new RawMaterialSafeInventoryPo("001","cotton",10000);
+        RawMaterialSafeInventoryPo po2 = new RawMaterialSafeInventoryPo("001","steel",20000);
+        ArrayList<RawMaterialSafeInventoryPo> list = new ArrayList<>();
+        list.add(po1);
+        list.add(po2);
+        inventoryService.SaveRawMaterialSafeInventory(list);
+    }
+
+    @Test
+    public void testSaveProductSafeInventory(){
+        ProductSafeInventoryPo po1 = new ProductSafeInventoryPo("001","pen",10000);
+        ProductSafeInventoryPo po2 = new ProductSafeInventoryPo("001","pencil",20000);
+        ArrayList<ProductSafeInventoryPo> list = new ArrayList<>();
+        list.add(po1);
+        list.add(po2);
+        inventoryService.SaveProductSafeInventory(list);
+    }
+
+    @Test
+    public void testgetAllRawMaterialVariety(){
+        ArrayList<String> list = inventoryService.getAllRawMaterialVariety("001");
+        for (String variety : list){
+            System.out.println(variety);
+        }
+    }
+
+    @Test
+    public void testgetAllProductVariety(){
+        ArrayList<String> list = inventoryService.getAllProductVariety("001");
+        for (String variety : list){
+            System.out.println(variety);
+        }
+    }
+
+    @Test
+    public void testgetAllRawMaterialSafeInventory(){
+        ArrayList<RawMaterialSafeInventoryPo> list = inventoryService.getAllRawMaterialSafeInventory("001");
+        for (RawMaterialSafeInventoryPo po : list){
+            print(po);
+        }
+    }
+
+    @Test
+    public void testgetAllProductSafeInventory(){
+        ArrayList<ProductSafeInventoryPo> list = inventoryService.getAllProductSafeInventory("001");
+        for (ProductSafeInventoryPo po : list){
+            print(po);
+        }
+    }
+
+    @Test
+    public void testgetRawInventory(){
+        System.out.println(inventoryService.getRawInventory("001","cotton"));
+    }
+
+    @Test
+    public void testgetProductInventory(){
+        System.out.println(inventoryService.getProductInventory("001","pesn"));
+    }
+
+    @Test
+    public void test(){
+        System.out.println(Boolean.valueOf("0"));
+    }
+
+    private void print(InventoryRawMaterialItemPO po){
+        System.out.print(po.getSheetID() + "\t");
+        System.out.print(po.getListID() + "\t");
+        System.out.print(po.getCompanyID() + "\t");
+        System.out.print(po.getVariety() + "\t");
+        System.out.print(po.getVoucherID() + "\t");
+        System.out.print(po.getDate() + "\t");
+        System.out.print(po.isDeliveryOntime() + "\t");
+        System.out.print(po.isReturn() + "\t");
+        System.out.print(po.getInputNum() + "\t");
+        System.out.print(po.getInputUnitPrice() + "\t");
+        System.out.print(po.getInputTotalPrice() + "\t");
+        System.out.print(po.getOutputNum() + "\t");
+        System.out.print(po.getOutputUnitPrice() + "\t");
+        System.out.print(po.getOutputTotalPrice() + "\t");
+        System.out.println(po.getRemainNum() + "\t");
+
+    }
+
+    private void print(InventoryProductItemPO po){
+        System.out.print(po.getSheetID() + "\t");
+        System.out.print(po.getListID() + "\t");
+        System.out.print(po.getCompanyID() + "\t");
+        System.out.print(po.getVariety() + "\t");
+        System.out.print(po.getVoucherID() + "\t");
+        System.out.print(po.getDate() + "\t");
+        System.out.print(po.isDeliveryOntime() + "\t");
+        System.out.print(po.isReturn() + "\t");
+        System.out.print(po.getInputNum() + "\t");
+        System.out.print(po.getInputUnitPrice() + "\t");
+        System.out.print(po.getInputTotalPrice() + "\t");
+        System.out.print(po.getOutputNum() + "\t");
+        System.out.print(po.getOutputUnitPrice() + "\t");
+        System.out.print(po.getOutputTotalPrice() + "\t");
+        System.out.println(po.getRemainNum() + "\t");
+
+    }
+
+    private void print(RawMaterialSafeInventoryPo po){
+        System.out.print(po.getCompany_id() + "\t");
+        System.out.print(po.getRaw_material_variety() + "\t");
+        System.out.println(po.getSafe_inventory()+ "\t");
+    }
+
+    private void print(ProductSafeInventoryPo po){
+        System.out.print(po.getCompany_id() + "\t");
+        System.out.print(po.getProduct_variety() + "\t");
+        System.out.println(po.getSafe_inventory() + "\t");
     }
 }
