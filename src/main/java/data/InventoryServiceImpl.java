e1b8a234e365c00b20d51f39befad7be550e1350
loohaze <loohaze529@gmail.com>
2017-09-11 00:05:17 +0800

    删除 三个商的PO 库存管理实现部分完成

--- src/main/java/data/InventoryServiceImpl.java
+++ src/main/java/data/InventoryServiceImpl.java
@@ -7,6 +7,8 @@ import po.Inventory.ProductSafeInventoryPo;
 import po.Inventory.RawMaterialSafeInventoryPo;
 
 import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
 
 /**
  * Created by loohaze on 2017/9/6 上午10:11
@@ -18,41 +20,70 @@ public class InventoryServiceImpl implements InventoryService{
 
     @Override
     public int getRawInventoryBySupplier(String company_id, String voucher_id, String raw_material_name) {
-        return 0;
+        sqlManager.getConnection();
+
+        String sql = "select * from inventory_material as t where t.datetime = (select max(datetime) from inventory_material where company_id=? and voucher_id=? and material_variety=?) and company_id=? and voucher_id=? and material_variety=?";
+        Map<String,Object> map = sqlManager.querySimple(sql,new Object[]{company_id,voucher_id,raw_material_name,company_id,voucher_id,raw_material_name});
+
+        int remain = Integer.parseInt(map.get("balance_num").toString());
+
+        sqlManager.releaseAll();
+        return remain;
     }
 
-    @Override
-    public boolean setSupplierRawInventory(String company_id, String voucher_id, String raw_material_name, int inventory) {
-        return false;
-    }
 
     @Override
     public int getProductInventoryByProducer(String company_id, String voucher_id, String product_name) {
-        return 0;
+        sqlManager.getConnection();
+
+        String sql = "select * from inventory_product as t where t.datetime = (select max(datetime) from inventory_product where company_id=? and voucher_id=? and product_variety=?) and company_id=? and voucher_id=? and product_variety=?";
+        Map<String,Object> map = sqlManager.querySimple(sql,new Object[]{company_id,voucher_id,product_name,company_id,voucher_id,product_name});
+
+        int remain = Integer.parseInt(map.get("balance_num").toString());
+
+        sqlManager.releaseAll();
+        return remain;
     }
 
-    @Override
-    public boolean setProducterProductInventory(String company_id, String voucher_id, String product_name, int inventory) {
-        return false;
-    }
 
     @Override
     public int getMaxSheetId(String company_id) {
-        return 0;
+        sqlManager.getConnection();
+
+        String sql = "select sheet_id from(select sheet_id from inventory_material where company_id = ? union select sheet_id from inventory_product where company_id=?) alias group by sheet_id order by sheet_id desc limit 1";
+        Map<String,Object> map = sqlManager.querySimple(sql,new Object[]{company_id,company_id});
+        int maxSheetID = Integer.parseInt(map.get("sheet_id").toString());
+
+        sqlManager.releaseAll();
+        return maxSheetID;
     }
 
     @Override
     public void SaveRawMaterialInventoryItem(ArrayList<InventoryRawMaterialItemPO> list) {
-
+        sqlManager.getConnection();
+        for (InventoryRawMaterialItemPO po : list){
+            insertOneInventoryRawMaterialItemPO(po);
+        }
+        sqlManager.releaseAll();
     }
 
     @Override
     public void SaveProductInventoryItem(ArrayList<InventoryProductItemPO> list) {
-
+        sqlManager.getConnection();
+        for (InventoryProductItemPO po : list){
+            insertOneInventoryProductItemPO(po);
+        }
+        sqlManager.releaseAll();
     }
 
     @Override
     public ArrayList<InventoryRawMaterialItemPO> getRawMaterialInventoryItem(String company_id, String time) {
+        sqlManager.getConnection();
+
+        ArrayList<InventoryRawMaterialItemPO> list = new ArrayList<>();
+
+        String sql = "select * from inventory_material where datetime<=? and company_id =? group by material_variety order by datetime";
+        sqlManager.releaseAll();
         return null;
     }
 
@@ -109,5 +140,51 @@ public class InventoryServiceImpl implements InventoryService{
     @Override
     public int getProductInventory(String company_id, String product_variety) {
         return 0;
+    }
+
+
+    private void insertOneInventoryRawMaterialItemPO(InventoryRawMaterialItemPO po){
+        List<Object> params = new ArrayList<>();
+
+        params.add(po.getSheetID());
+        params.add(po.getListID());
+        params.add(po.getCompanyID());
+        params.add(po.getVariety());
+        params.add(po.getVoucherID());
+        params.add(po.getDate());
+        params.add(po.isDeliveryOntime());
+        params.add(po.isReturn());
+        params.add(po.getInputNum());
+        params.add(po.getInputUnitPrice());
+        params.add(po.getInputTotalPrice());
+        params.add(po.getOutputNum());
+        params.add(po.getOutputUnitPrice());
+        params.add(po.getOutputTotalPrice());
+        params.add(po.getRemainNum());
+
+        String sql = sqlManager.appendSQL("insert into inventory_material values",params.size());
+        sqlManager.executeUpdateByList(sql,params);
+    }
+
+    private void insertOneInventoryProductItemPO(InventoryProductItemPO po){
+        List<Object> params = new ArrayList<>();
+
+        params.add(po.getSheetID());
+        params.add(po.getListID());
+        params.add(po.getCompanyID());
+        params.add(po.getVariety());
+        params.add(po.getVoucherID());params.add(po.getDate());
+        params.add(po.isDeliveryOntime());
+        params.add(po.isReturn());
+        params.add(po.getInputNum());
+        params.add(po.getInputUnitPrice());
+        params.add(po.getInputTotalPrice());
+        params.add(po.getOutputNum());
+        params.add(po.getOutputUnitPrice());
+        params.add(po.getOutputTotalPrice());
+        params.add(po.getRemainNum());
+
+        String sql = sqlManager.appendSQL("insert into inventory_product values",params.size());
+        sqlManager.executeUpdateByList(sql,params);
     }
 }
--- src/main/test/dataTest/InventoryDataServiceImplTest.java
+++ src/main/test/dataTest/InventoryDataServiceImplTest.java
@@ -3,6 +3,14 @@ package dataTest;
 import data.InventoryServiceImpl;
 import dataservice.InventoryService;
 import org.junit.Before;
+import org.junit.Test;
+import po.Inventory.InventoryProductItemPO;
+import po.Inventory.InventoryRawMaterialItemPO;
+
+import java.sql.Timestamp;
+import java.util.ArrayList;
+
+import static junit.framework.TestCase.assertEquals;
 
 /**
  * Created by loohaze on 2017/9/6 上午10:14
@@ -17,4 +25,36 @@ public class InventoryDataServiceImplTest {
     }
 
 
+    @Test
+    public void testGetRawInventoryBySupplier(){
+        assertEquals(30,inventoryService.getRawInventoryBySupplier("001","借-1","cotton"));
+    }
+
+    @Test
+    public void testGetMaxSheetID(){
+        System.out.println(inventoryService.getMaxSheetId("001"));
+    }
+
+    @Test
+    public void testSaveRawMaterialInventoryItem(){
+        ArrayList<InventoryRawMaterialItemPO> list = new ArrayList<>();
+
+        InventoryRawMaterialItemPO po1 = new InventoryRawMaterialItemPO(1,0,"001","steel","记-1", Timestamp.valueOf("2017-09-10 00:00:00"),false,false,100,100,10000,0,0,0,100);
+        InventoryRawMaterialItemPO po2 = new InventoryRawMaterialItemPO(1,1,"001","xxx","记-1",Timestamp.valueOf("2017-09-10 00:00:00"),false,false,200,100,20000,0,0,0,200);
+        list.add(po1);
+        list.add(po2);
+
+        inventoryService.SaveRawMaterialInventoryItem(list);
+    }
+
+
+    @Test
+    public void testSaveProductInventoryItem(){
+        ArrayList<InventoryProductItemPO> list = new ArrayList<>();
+
+        InventoryProductItemPO po1 = new InventoryProductItemPO(3,0,"001","pen","记-3", Timestamp.valueOf("2017-09-11 00:00:00"),false,false,0,0,0,1000,10,10000,0);
+        list.add(po1);
+
+        inventoryService.SaveProductInventoryItem(list);
+    }
 }
