ba78dfffc14ed19b6ae8d39c6b8928f5c4057f93
loohaze <loohaze529@gmail.com>
2017-09-12 20:52:30 +0800

    CreditItemServiceImpl 实现

--- src/main/java/data/CreditItemServiceImpl.java
+++ src/main/java/data/CreditItemServiceImpl.java
@@ -3,6 +3,7 @@ package data;
 import dataservice.CreditItemService;
 import po.CreditItemPO;
 
+import java.sql.Date;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
@@ -29,19 +30,29 @@ public class CreditItemServiceImpl implements CreditItemService{
         sqlManager.getConnection();
         ArrayList<CreditItemPO> list = new ArrayList<>();
 
-        String sql = "select * from credit_item where company_id=? and voucher_id=?";
+        String sql = "select * from credit_item where company_id=? and voucher_id=? and type='应收账款'";
         ArrayList<Map<String,Object>> maps = sqlManager.queryMulti(sql,new Object[]{company_id,voucher_id});
         for (Map<String,Object> map : maps){
-
+            list.add(getCreditItemPOByMap(map));
         }
 
         sqlManager.releaseAll();
-        return null;
+        return list;
     }
 
     @Override
     public ArrayList<CreditItemPO> getAccountsPayable(String company_id, String voucher_id) {
-        return null;
+        sqlManager.getConnection();
+        ArrayList<CreditItemPO> list = new ArrayList<>();
+
+        String sql = "select * from credit_item where company_id=? and voucher_id=? and type='应付账款'";
+        ArrayList<Map<String,Object>> maps = sqlManager.queryMulti(sql,new Object[]{company_id,voucher_id});
+        for (Map<String,Object> map : maps){
+            list.add(getCreditItemPOByMap(map));
+        }
+
+        sqlManager.releaseAll();
+        return list;
     }
 
 
@@ -64,6 +75,18 @@ public class CreditItemServiceImpl implements CreditItemService{
     }
 
     private CreditItemPO getCreditItemPOByMap(Map<String,Object> map){
-        return null;
+        CreditItemPO po = new CreditItemPO();
+        po.setType(map.get("type").toString());
+        po.setItemID(map.get("item_id").toString());
+        po.setVoucherID(map.get("voucher_id").toString());
+        po.setCompanyName(map.get("company_name").toString());
+        po.setDebitDate(Date.valueOf(map.get("debit_date").toString()));
+        po.setDdl(Date.valueOf(map.get("ddl").toString()));
+        po.setCreditNum(Double.parseDouble(map.get("credit_num").toString()));
+        po.setDiscountPolicy(map.get("discount_policy").toString());
+        po.setDebitDate(Date.valueOf(map.get("discount_date").toString()));
+        po.setComment(map.get("comment").toString());
+
+        return po;
     }
 }
