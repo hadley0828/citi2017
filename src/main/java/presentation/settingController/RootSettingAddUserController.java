23931c3adc9fee044c5f7020802c72faff01f065
151250034 <151250034@smail.nju.edu.cn>
2017-09-12 20:23:48 +0800

    设置界面基本完成

---
+++ src/main/java/presentation/settingController/RootSettingAddUserController.java
@@ -0,0 +1,28 @@
+package presentation.settingController;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+
+/**
+ * Created by Chris on 2017/9/12.
+ */
+public class RootSettingAddUserController {
+    @FXML
+    private Label id;
+    @FXML
+    private Label password;
+    @FXML
+    private Button closeButton;
+    @FXML
+    public void initialize(){
+
+    }
+
+    public void CloseClicked(){
+        /**
+         * 关闭当前窗口
+         */
+    }
+
+}
