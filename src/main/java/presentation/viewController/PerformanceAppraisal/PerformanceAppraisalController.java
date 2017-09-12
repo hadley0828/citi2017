5ef9fc1a53d3d80167f746a6b1f9c86479bc88da
YZ <1150681747@qq.com>
2017-09-12 16:49:44 +0800

    绩效评价表格数据

--- src/main/java/presentation/viewController/PerformanceAppraisal/PerformanceAppraisalController.java
+++ src/main/java/presentation/viewController/PerformanceAppraisal/PerformanceAppraisalController.java
@@ -1,10 +1,18 @@
 package presentation.viewController.PerformanceAppraisal;
 
+import businesslogic.SupplyChainImpl;
+import businesslogicservice.SupplyChainService;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
+import presentation.viewController.CashSheet.CashVO;
+import vo.SupplyChainPerformanceVo;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
 
 /**
  * Created by YZ on 2017/9/9.
@@ -69,6 +77,8 @@ public class PerformanceAppraisalController {
     @FXML
     private TableColumn debtpay_balance;
 
+    private SupplyChainService service=new SupplyChainImpl();
+    SupplyChainPerformanceVo vo=service.SupplyChain_Supplier("001","002","003","date");
     @FXML
     public void initialize(){
         setGain();
@@ -78,19 +88,67 @@ public class PerformanceAppraisalController {
     }
 
     public void setGain(){
+        ArrayList<ProfitAbilityVO> profitAbilityVOS=new ArrayList<ProfitAbilityVO>();
+        profitAbilityVOS.add(new ProfitAbilityVO("供应商",vo.getSupplier()[0][0],vo.getSupplier()[0][1]));
+        profitAbilityVOS.add(new ProfitAbilityVO("生产商",vo.getManufacturer()[0][0],vo.getManufacturer()[0][1]));
+        profitAbilityVOS.add(new ProfitAbilityVO("分销商",vo.getDistributor()[0][0],vo.getDistributor()[0][1]));
 
+        ObservableList<ProfitAbilityVO> p_list= FXCollections.observableArrayList(profitAbilityVOS);
+
+        gain_enterprise.setCellValueFactory(new PropertyValueFactory("enterprise"));
+        gain_sumPayback.setCellValueFactory(new PropertyValueFactory("property_pay"));
+        gain_benefit.setCellValueFactory(new PropertyValueFactory("market_purify"));
+
+        gain_table.setItems(p_list);
     }
 
     public void setOp(){
+        //准时交货率和退货率有问题
+        ArrayList<OperationVO> operationVOS=new ArrayList<OperationVO>();
+        operationVOS.add(new OperationVO("供应商",vo.getSupplier()[1][0],vo.getSupplier()[1][1],vo.getSupplier()[1][0],vo.getSupplier()[1][1]));
+        operationVOS.add(new OperationVO("生产商",vo.getManufacturer()[1][0],vo.getManufacturer()[1][1],vo.getManufacturer()[1][0],vo.getManufacturer()[1][1]));
+        operationVOS.add(new OperationVO("分销商",vo.getDistributor()[1][0],vo.getDistributor()[1][1],vo.getDistributor()[1][0],vo.getDistributor()[1][1]));
+
+        ObservableList<OperationVO> o_list=FXCollections.observableList(operationVOS);
+
+        op_enterprise.setCellValueFactory(new PropertyValueFactory("enterprise"));
+        op_sum.setCellValueFactory(new PropertyValueFactory("property_rotate"));
+        op_stock.setCellValueFactory(new PropertyValueFactory("stock_rotate"));
+        op_ontime.setCellValueFactory(new PropertyValueFactory("ontime"));
+        op_back.setCellValueFactory(new PropertyValueFactory("back"));
+
+        op_table.setItems(o_list);
 
     }
 
     public void setDevelop(){
+        ArrayList<DevelopVO> developVOS=new ArrayList<DevelopVO>();
+        developVOS.add(new DevelopVO("供应商",vo.getSupplier()[2][0],vo.getSupplier()[2][1]));
+        developVOS.add(new DevelopVO("生产商",vo.getManufacturer()[2][0],vo.getManufacturer()[2][1]));
+        developVOS.add(new DevelopVO("分销商",vo.getDistributor()[2][0],vo.getDistributor()[2][1]));
+
+        ObservableList<DevelopVO> d_list=FXCollections.observableList(developVOS);
+
+        develop_enterprise.setCellValueFactory(new PropertyValueFactory("enterprise"));
+        develop_increase.setCellValueFactory(new PropertyValueFactory("market_increase"));
+        develop_profit.setCellValueFactory(new PropertyValueFactory("profit_increase"));
+
+        develop_table.setItems(d_list);
 
     }
 
     public void setDebtpay(){
+        ArrayList<PayDebtVO> payDebtVOS=new ArrayList<PayDebtVO>();
+        payDebtVOS.add(new PayDebtVO("供应商",vo.getSupplier()[3][0]));
+        payDebtVOS.add(new PayDebtVO("生产商",vo.getManufacturer()[3][0]));
+        payDebtVOS.add(new PayDebtVO("分销商",vo.getDistributor()[3][0]));
 
+        ObservableList<PayDebtVO> pay_list=FXCollections.observableList(payDebtVOS);
+
+        debtpay_enterprise.setCellValueFactory(new PropertyValueFactory("enterprise"));
+        debtpay_debt.setCellValueFactory(new PropertyValueFactory("balance"));
+
+        debtpay_table.setItems(pay_list);
     }
 
 }
