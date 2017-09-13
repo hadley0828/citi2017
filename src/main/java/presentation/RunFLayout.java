cee7709e4c1cb6d51bba8745af53d6dc0b70913a
151250034 <151250034@smail.nju.edu.cn>
2017-09-13 19:32:10 +0800

    设置界面

---
+++ src/main/java/presentation/RunFLayout.java
@@ -0,0 +1,26 @@
+package presentation;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+/**
+ * @author Molloh
+ * @version 2017/9/6
+ */
+public class RunFLayout extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("../view/supplyChainManagement/financialInstitutions/FinancialInstitutions.fxml"));
+        Scene scene = new Scene(root);
+
+        primaryStage.setMinWidth(1000);
+        primaryStage.setMinHeight(600);
+        primaryStage.setScene(scene);
+
+        primaryStage.show();
+    }
+}
