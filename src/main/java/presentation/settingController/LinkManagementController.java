a545fc0d075000aff4525ea02eb0b97ef4c7b719
151250034 <151250034@smail.nju.edu.cn>
2017-09-14 19:20:09 +0800

    shezhi

--- src/main/java/presentation/settingController/LinkManagementController.java
+++ src/main/java/presentation/settingController/LinkManagementController.java
@@ -1,14 +1,18 @@
 package presentation.settingController;
 
+import businesslogic.SettingImpl;
 import businesslogic.SupplyChainImpl;
 import businesslogic.UserManagementImpl;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.RadioButton;
 import javafx.scene.control.TextField;
+import javafx.stage.Stage;
 import presentation.screenController.ControlledScreen;
 import presentation.screenController.ScreensController;
 import presentation.StaticFactory;
+import presentation.warningController.RunWarning;
+
 import vo.userManagement.AccountSetVO;
 import vo.userManagement.UserVO;
 
@@ -42,17 +46,25 @@ public class LinkManagementController implements ControlledScreen {
         String place=accountvo.getChainPlace();
         if(place.equals("生产商")){
             product.setSelected(true);
+            sale.setDisable(true);
+            supply.setDisable(true);
             up.setText(companys[0]);
             down.setText(companys[2]);
         }else if(place.equals("分销商")){
             sale.setSelected(true);
+            supply.setDisable(true);
+            product.setDisable(true);
             up.setText(companys[1]);
+            down.setDisable(true);
 
 
 
         }else if (place.equals("供应商")){
             supply.setSelected(true);
+            sale.setDisable(true);
+            product.setDisable(true);
             down.setText(companys[1]);
+            up.setDisable(true);
 
         }
 
@@ -66,7 +78,37 @@ public class LinkManagementController implements ControlledScreen {
     }
 
     public void saveClicked(){
-        int type=0;
+        String  type="";
+        if(supply.isSelected()){
+            type="供应商";
+        }else if(product.isSelected()){
+            type="生产商";
+        }else if(sale.isSelected()){
+            type="分销商";
+        }
+        String upper=up.getText();
+        String downner=down.getText();
+        if(upper.isEmpty()){
+            upper.equals("");
+        }
+        if(downner.isEmpty()){
+            downner.equals("");
+        }
+        UserVO vo= StaticFactory.getUserVO();
+        SettingImpl ipl=new SettingImpl();
+        Boolean rm=ipl.setSupplyChain(vo.getCompanyID(),type,upper,downner);
+        if(rm==false){
+            RunWarning runWarning=new RunWarning();
+            runWarning.SetWarning("修改失败！");
+            runWarning.start(new Stage());
+        }else if(rm==true){
+            RunWarning runWarning=new RunWarning();
+            runWarning.SetWarning("修改成功！");
+            runWarning.start(new Stage());
+
+        }
+
+
 
 
 
@@ -77,17 +119,23 @@ public class LinkManagementController implements ControlledScreen {
     }
 
     public void supplyClicked(){
-        product.setSelected(false);
-        sale.setSelected(false);
+        product.setSelected(true);
+        sale.setSelected(true);
+        up.setDisable(true);
+        down.setDisable(false);
 
     }
     public void productClicked(){
-        sale.setSelected(false);
-        supply.setSelected(false);
+        sale.setSelected(true);
+        supply.setSelected(true);
+        up.setDisable(false);
+        down.setDisable(false);
     }
     public void saleClicked(){
-        supply.setSelected(false);
-        product.setSelected(false);
+        supply.setSelected(true);
+        product.setSelected(true);
+        up.setDisable(false);
+        down.setDisable(true);
 
     }
     @Override
--- src/main/java/presentation/settingController/RootSettingController.java
+++ src/main/java/presentation/settingController/RootSettingController.java
@@ -1,11 +1,19 @@
 package presentation.settingController;
 
+import businesslogic.UserManagementImpl;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
+import presentation.StaticFactory;
 import presentation.screenController.ControlledScreen;
 import presentation.screenController.ScreensController;
+import vo.userManagement.UserVO;
+
+import java.util.ArrayList;
 
 
 /**
@@ -16,14 +24,23 @@ public class RootSettingController implements ControlledScreen{
     private Button addUserButton;
 
     @FXML
-    private TableView<Object> userTable;
+    private TableView<UserVO> userTable;
     @FXML
-    private TableColumn<Object,String> userInfo;
+    private TableColumn<UserVO,String> userInfo;
     @FXML
-    private TableColumn<Object,String> userRoot;
+    private TableColumn<UserVO,String> userRoot;
 
     @FXML
     public void initialize(){
+        UserVO vo= StaticFactory.getUserVO();
+        String accId=vo.getAccountID();
+        ArrayList<UserVO> list=new UserManagementImpl().getAllUserVoByAccountId(accId);
+        ObservableList<UserVO> data= FXCollections.observableArrayList(list);
+        userTable.setItems(data);
+        userInfo.setCellValueFactory(new PropertyValueFactory("UserID"));
+        userRoot.setCellValueFactory(new PropertyValueFactory("type"));
+
+
         //显示uservo数量
 
 
