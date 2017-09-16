f331a4d36bea3f4a87bee8d01ba77bea0b8c25e7
151250034 <151250034@smail.nju.edu.cn>
2017-09-16 11:08:31 +0800

    setting

--- src/main/java/presentation/settingController/SafeCell.java
+++ src/main/java/presentation/settingController/SafeCell.java
@@ -24,33 +24,14 @@ public class SafeCell extends TableCell<SafeInventoryVo,Boolean> {
 
 
     SafeCell(){
-//        cellButton.setOnAction(new EventHandler<ActionEvent>(){
-//            @Override
-//            public void handle(ActionEvent t) {
-////                	viewcontrol.OrderClicked();
-//                try{
-//                    OrderVO ordervo = getTableView().getItems().get( getIndex() );
-//                    String hotelid=ordervo.getHotel_id().get();
-//                    HotelController hotelController=new HotelController();
-//                    viewcontrol.hotelLevel.setText(hotelController.getNmaeById(hotelid));
-//                    viewcontrol.businessCircle.setText(hotelController.viewHotel(hotelController.getNmaeById(hotelid)).getHotelArea().get());
-//                    viewcontrol.checkinTime.setText(ordervo.getExecuteTime().get());
-//                    viewcontrol.roomType.setText(ordervo.getRoomType().get());
-//                    viewcontrol.roomNum.setText(Integer.toString(ordervo.getNumber().get()));
-//                    viewcontrol.peopleNum.setText(Integer.toString(ordervo.getPeople().get()));
-//                    viewcontrol.price.setText(Integer.toString(ordervo.getOrderPrice().get()));
-//                }catch(Exception e){
-//                    e.printStackTrace();
-//                }
-//            }
-//        });
 
-        cellText.setText("");
+        SafeInventoryVo vo = getTableView().getItems().get(getIndex());
+
+        cellText.setText(vo.getVariety());
         cellText.textProperty().addListener(new ChangeListener<String>() {
             @Override
             public void changed(ObservableValue<? extends String> observable, String oldValue, String newValue) {
-                SafeInventoryVo vo = getTableView().getItems().get(getIndex());
-//                getTableView().getColumns().get(0).getCellFactory().call();
+
             }
 
 
